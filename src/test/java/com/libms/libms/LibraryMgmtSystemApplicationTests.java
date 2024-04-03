2404fee457c71be1ab8d7f9df68dc5320d755e81
NidaaHamed <nidaasallam@gmail.com>
2024-04-03 23:45:56 +0200

    initial commit

---
+++ src/test/java/com/libms/libms/LibraryMgmtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libms.libms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMgmtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
